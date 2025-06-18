65941086e29ec7204e96d82c74d365fca319b921
kepter <tuna26hn@gmail.com>
2025-06-19 05:08:27 +0700

    first commit

---
+++ src/main/java/com/example/SpringExaminationSystem/SpringExaminationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringExaminationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExaminationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExaminationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringExaminationSystem/SpringExaminationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringExaminationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExaminationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
